ecf9bdc4deb4a1b5799c551ab45259ebbd0611d7
ApothecaryOfLight <changerofways@gmail.com>
2022-06-11 14:35:55 -0400

    Removed deprecated code and unnecessary logging.

--- test/test.js
+++ test/test.js
@@ -25,29 +25,7 @@ function recursively_traverse_tree( node, objectified_post ) {
 
 function get_text() {
     const objectified_post = [];
-
     const myInput = document.getElementById("myInput");
-
-    console.dir( myInput.childNodes );
-    
-    /*for( const key in myInput.childNodes ) {
-        const element = myInput.childNodes[key];
-        const type = element.nodeName;
-
-        if( type == "#text" || type == "DIV" ) {
-            if( element.textContent ) {
-                console.log( element.textContent );
-                objectified_post.push( element.textContent );
-            }
-        } else if( type == "IMG" ) {
-            objectified_post.push({
-                src: element.src,
-                name: "image name",
-                alt: "alt desc"
-            });
-        }
-    };*/
     recursively_traverse_tree( myInput, objectified_post );
-
     console.dir( objectified_post );
 }
