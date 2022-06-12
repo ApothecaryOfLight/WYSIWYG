91d38aa225c60900bfd6099f1a7f93a360d24533
ApothecaryOfLight <changerofways@gmail.com>
2022-06-12 10:33:01 -0400

    Adjusted overlay button position and composition.

--- blog_edit_gallery/blog_edit_gallery_image_overlay.js
+++ blog_edit_gallery/blog_edit_gallery_image_overlay.js
@@ -2,16 +2,16 @@ function create_gallery_image_overlay( image_container_ref, image_ref ) {
     const overlay = document.createElement("div");
     overlay.classList = "blog_edit_gallery_image_control_overlay";
   
-    const delete_button = document.createElement("button");
-    delete_button.classList = "blog_edit_gallery_image_control_overlay_delete_button";
+    const delete_button = document.createElement("div");
+    delete_button.classList = "blod_edit_gallery_image_control_overlay_button";
     delete_button.textContent = "X";
     delete_button.onclick = () => {
       image_container_ref.remove();
     }
     overlay.appendChild( delete_button );
   
-    const emplace_button = document.createElement("button");
-    emplace_button.classList = "blog_edit_gallery_image_control_overlay_emplace_button";
+    const emplace_button = document.createElement("div");
+    emplace_button.classList = "blod_edit_gallery_image_control_overlay_button";
     emplace_button.textContent = "<-";
     emplace_button.onclick = () => {
       const image_element = document.createElement("img");
